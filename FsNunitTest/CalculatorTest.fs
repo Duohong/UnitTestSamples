f95118e3776a160fb5c3217fe9973f15b38bb156
Duohong <duohongopen@gmail.com>
2019-01-18 18:34:43 -0500

    add Fs NUnit sample

---
+++ FsNunitTest/CalculatorTest.fs
@@ -0,0 +1,30 @@
+﻿namespace FsNunitTest
+
+open NUnit.Framework
+
+[<TestFixture>]
+type CalculatorTest () =
+    let mCalculator = new Production.Calculator()
+
+    [<SetUp>]
+    member this.Setup () =
+        ()
+
+    [<Test>]
+    member this.AddTest () =
+        Assert.AreEqual(mCalculator.Add(2, 3), 5)
+
+    [<Test>]
+    member this.FailingTest () =
+        Assert.AreEqual(5, 4)
+
+    [<TestCase(3, 4, 7 )>]
+    [<TestCase(5, 6, 11)>]
+    [<TestCase(5, 6, 12)>]
+    member this.Add_Expect (a : int, b : int, expected : int) =
+        Assert.AreEqual(expected, mCalculator.Add(a, b));
+
+    [<Test>]
+    [<Pairwise>]
+    member this.ColumnParameterTest ([<Values(1, 2, 3)>] (i : int), [<Values("a", "b", "c")>] (s : string)) =
+        Assert.Greater(i, 0)
